21a6972c6bf250b7a59c47271dbf135481b7f787
Aleksey Chistyakov <chskela@gmail.com>
2020-05-12 22:43:53 +0300

    5 of 18 core exercises completed successfully, Side Exercises completed 11/85, done dnd-character

---
+++ dnd-character/dnd-character.js
@@ -0,0 +1,57 @@
+//
+// This is only a SKELETON file for the 'D&D Character' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const abilityModifier = n => {
+  if (n < 3) {
+    throw new Error('Ability scores must be at least 3');
+  };
+  if (n > 18) {
+    throw new Error('Ability scores can be at most 18');
+  }
+  return Math.floor((n - 10) / 2);
+};
+
+export class Character {
+  constructor() {
+    this._strength = Character.rollAbility();
+    this._dexterity = Character.rollAbility();
+    this._constitution = Character.rollAbility();
+    this._intelligence = Character.rollAbility();
+    this._wisdom = Character.rollAbility();
+    this._charisma = Character.rollAbility();
+  }
+
+  static rollAbility() {
+    return Math.round(2.5 + Math.random() * 16);
+  }
+
+  get strength() {
+    return this._strength;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  get constitution() {
+    return this._constitution;
+  }
+
+  get intelligence() {
+    return this._intelligence;
+  }
+
+  get wisdom() {
+    return this._wisdom;
+  }
+
+  get charisma() {
+    return this._charisma;
+  }
+
+  get hitpoints() {
+    return 10 + abilityModifier(this.constitution);
+  }
+}

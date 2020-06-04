158da65b14558c01051e30e63c0f9465b1692692
Aleksey Chistyakov <chskela@gmail.com>
2020-06-04 23:54:03 +0300

    6 of 18 core exercises completed successfully, Side Exercises compleed 15/85, done rna-transcriptiont

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,25 +1,9 @@
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const toRna = (dna) => {
-  let rna = '';
-  for (const char of dna) {
-    switch (char) {
-      case 'G':
-        rna += 'C';
-        break;
-      case 'C':
-        rna += 'G';
-        break;
-      case 'T':
-        rna += 'A';
-        break;
-      case 'A':
-        rna += 'U';
-        break;
-    }
-  }
-  return rna;
+  const match = {
+    G: 'C',
+    C: 'G',
+    T: 'A',
+    A: 'U'
+  };
+return dna.split('').map(i => match.hasOwnProperty(i) && match[i]).join('');
 };

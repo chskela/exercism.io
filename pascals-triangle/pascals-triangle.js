e90107fb07cfc49503a378fa5f4b86772ebeaf0a
Aleksey Chistyakov <chskela@gmail.com>
2021-01-22 14:03:32 +0300

    11 of 18 core exercises completed successfully, Side Exercises compleed 52/89, fix pascals-triangle

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -1,11 +1,17 @@
-export const rows = (i) => {
-  const arr = [];
-  for (let n = 0; n < i; n++) {
-    const row = [];
-    for (let k = 0; k <= n; k++) {
-      row[k] = (k === 0 || k === n) ? 1 : arr[n - 1][k - 1] + arr[n - 1][k];
-    }
-    arr[n] = row;
+export const rows = (n) => {
+  if (n === 0) {
+    return [];
   }
-  return arr;
+  const triangle = [[1]];
+  for (let i = 1; i < n; i++) {
+    const row = [];
+    for (let j = 0; j <= i; j++) {
+      const next = triangle[i - 1];
+      const previousLeft = next[j - 1] || 0;
+      const previousRight = next[j] || 0;
+      row[j] = previousLeft + previousRight;
+    }
+    triangle[i] = row;
+  }
+  return triangle;
 };
